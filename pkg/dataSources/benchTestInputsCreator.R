a4630048a95d5c104eef336809aa998d1982aa35
davidorme <davidorme@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-11 09:59:45 +0000

    fixing bench test inputs: omitted colnames in test
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/caper@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dataSources/benchTestInputsCreator.R
+++ pkg/dataSources/benchTestInputsCreator.R
@@ -2,6 +2,7 @@ benchTreeDicho <- read.tree('BenchTreeDi.tre')
 benchTreePoly <- read.tree('BenchTreePoly.tre')
 benchData <-read.delim('BenchData.txt')
 testTree <- read.nexus('test.nex')
-testData <- read.delim('test.dat')
+testData <- read.delim('test.dat', header=FALSE)
+names(testData) <- c('tips','V1','V2')
 
 save(benchTreeDicho, benchTreePoly, benchData, testTree, testData, file="../data/benchTestInputs.rda")

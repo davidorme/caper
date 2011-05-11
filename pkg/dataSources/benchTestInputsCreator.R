38e27376e5f12a08270cf5016dfa1a93aecfa96f
davidorme <davidorme@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-11 10:16:24 +0000

    fixing bench test inputs: three level factors not ordered
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/caper@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dataSources/benchTestInputsCreator.R
+++ pkg/dataSources/benchTestInputsCreator.R
@@ -1,6 +1,9 @@
 benchTreeDicho <- read.tree('BenchTreeDi.tre')
 benchTreePoly <- read.tree('BenchTreePoly.tre')
 benchData <-read.delim('BenchData.txt')
+benchData$triFact <- ordered(benchData$triFact)
+benchData$triFactNA <- ordered(benchData$triFactNA)
+
 testTree <- read.nexus('test.nex')
 testData <- read.delim('test.dat', header=FALSE)
 names(testData) <- c('tips','V1','V2')
